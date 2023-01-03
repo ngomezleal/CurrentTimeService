7982c3385e779cdacd58c5c1041b0b5913634649
Nelson Gomez <ngomezleal@hotmail.com>
2023-01-03 14:21:13 -0500

    Commit to new development branch

--- CurrentTimeService/Controllers/WeatherForecastController.cs
+++ CurrentTimeService/Controllers/WeatherForecastController.cs
@@ -25,7 +25,8 @@ namespace CurrentTimeService.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                Origin = "Cabimas / Vzla"
             })
             .ToArray();
         }
