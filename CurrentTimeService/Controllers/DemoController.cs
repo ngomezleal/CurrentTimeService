4354a83d914b81b26629fb74fa8dc83551691881
Nelson Gomez <ngomezleal@hotmail.com>
2023-01-03 13:55:30 -0500

    Add new controller "Demo"

---
+++ CurrentTimeService/Controllers/DemoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrentTimeService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public List<string> Get()
+        {
+            List<string> list = new List<string>() 
+            {
+                new string("Nelson"),
+                new string("Isaac"),
+                new string("maidely")
+            };
+
+            return list;
+        }
+    }
+}
