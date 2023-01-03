79331226b4cdf1fecb6827fbc44a7066737d6693
Nelson Gomez <ngomezleal@hotmail.com>
2023-01-03 13:49:01 -0500

    Testing this api with minimum options 2

--- CurrentTimeService/Program.cs
+++ CurrentTimeService/Program.cs
@@ -9,19 +9,14 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-app.UseSwagger();
-app.UseSwaggerUI();
+Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
-
-////GET UTC
-//app.MapGet("time/utc", () => Results.Ok(DateTime.UtcNow));
 
 app.UseAuthorization();
 
